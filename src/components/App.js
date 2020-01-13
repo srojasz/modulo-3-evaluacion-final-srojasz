9935e4249574cec598b4893d9bac4879ed9da83e
srojasz <srojasz@gmail.com>
2020-01-13 08:25:03 +0100

    Install create-react-app & clean project

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>Hola, cómo estás</p>
+      </div>
+    );
+  }
+}
+
+export default App;
