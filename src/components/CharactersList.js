84ac37622400fbc9cead4b6857c3c54673e45c1b
srojasz <srojasz@gmail.com>
2020-01-13 09:48:56 +0100

    Add no result message into CharactersList

--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -2,16 +2,24 @@ import React from 'react'
 import CharacterCard from "./CharacterCard";
 
 function CharactersList(props) {
-  return <ul>
-    {props.characters.map((character, key) =>
-      <li key={character.id}>
-        <CharacterCard
-          img={character.image}
-          name={character.name}
-          specie={character.species} />
-      </li>
-    )}
-  </ul>
-}
 
+  if (props.characters.length === 0) {
+    return (
+      <p>Lo siento, no hemos encontrado resultados</p>
+    )
+
+  } else {
+
+    return <ul>
+      {props.characters.map((character, key) =>
+        <li key={character.id}>
+          <CharacterCard
+            img={character.image}
+            name={character.name}
+            specie={character.species} />
+        </li>
+      )}
+    </ul>
+  }
+}
 export default CharactersList;
